95068eb3b134debaa72adcd8288a343e6fe877a0
jovisly <j@ovisly.com>
2023-12-10 19:21:04 -0800

    templates

---
+++ templates/part1.py
@@ -0,0 +1,18 @@
+def solve(filename):
+    lines = open(filename, encoding="utf-8").read().splitlines()
+    # Stuff here.
+    return 0
+
+
+def mini_test():
+    filename = "input-test.txt"
+    assert solve(filename) == 0
+
+
+if __name__ == "__main__":
+    mini_test()
+
+    filename = "input.txt"
+    total = solve(filename)
+
+    print(total)
