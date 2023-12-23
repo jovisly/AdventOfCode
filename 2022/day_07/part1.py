33da10615a111cfd9f1568bd8e8a20a251771432
jovisly <j@ovisly.com>
2023-12-22 16:45:04 -0800

    still too low T_T

--- 2022/day_07/part1.py
+++ 2022/day_07/part1.py
@@ -1,6 +1,5 @@
-
 class Blob:
-    def __init__(self, type, name, parent):
+    def __init__(self, type, name, parent, depth):
         # A blob can be a directory ("d") or a file ("f").
         self.type = type
         self.name = name
@@ -8,83 +7,81 @@ class Blob:
         # Children are also blobs.
         self.children = set()
         self.size = -1
+        # Depth 0 is root, 1 is root's children, etc. This is to dedupe on the
+        # name level.
+        self.depth = depth
 
 
 
-def process_cd(line, dict_blobs, curr_blob):
+def process_cd(line, dict_blobs, curr_blob, depth):
     command = line[2:].split(" ")
     dest = command[1]
-    # if curr_blob is not None:
-    #     print("curr")
-    #     print(curr_blob.__dict__)
     if dest == "..":
-        # cd .. when we are already at root does nothing.
-        # if curr_blob.parent is not None:
-        curr_blob = dict_blobs[curr_blob.parent]
+        depth -= 1
+        curr_blob = dict_blobs[(curr_blob.parent, depth)]
+    elif dest == "/":
+        depth = 0
+        if ("/", depth) not in list(dict_blobs):
+            dict_blobs[("/", depth)] = Blob("d", "/", None, depth)
+        curr_blob = dict_blobs[("/", depth)]
     else:
-        if dest in list(dict_blobs):
-            curr_blob = dict_blobs[dest]
-        else:
-            if dest == "fqg":
-                print("======= WHAT IS GOING ON =======")
-                print(curr_blob.__dict__)
-                print(line)
-            parent = None if dest == "/" else curr_blob.name
-            curr_blob = Blob("d", dest, parent)
-            dict_blobs[dest] = curr_blob
+        depth += 1
+        if (dest, depth) not in list(dict_blobs):
+            dict_blobs[(dest, depth)] = Blob("d", dest, curr_blob.name, depth)
+        curr_blob = dict_blobs[(dest, depth)]
 
-    return dict_blobs, curr_blob
+    return dict_blobs, curr_blob, depth
 
 
 
-def process_ls(line, dict_blobs, curr_blob):
+def process_ls(line, dict_blobs, curr_blob, depth):
     result = line.split(" ")
+    # When we do ls, the depth of the new blobs is 1 more than the current.
+    new_depth = depth + 1
     if result[0] == "dir":
         # Add a directory if it doesn't already exist.
         dirname = result[1]
-        if dirname not in list(dict_blobs):
-            if dirname == "fqg":
-                print("======= WHAT IS GOING ON =======")
-                print(curr_blob.__dict__)
-                print(line)
-                print("~~~~~~~")
-
-            dict_blobs[dirname] = Blob("d", dirname, curr_blob.name)
+        if (dirname, new_depth) not in list(dict_blobs):
+            dict_blobs[(dirname, new_depth)] = Blob("d", dirname, curr_blob.name, new_depth)
         # We also want to denote that the directory is a child of the current blob.
-        curr_blob.children.add(dirname)
+        curr_blob.children.add((dirname, new_depth))
     else:
         # Otherwise we have a file with a file size.
         size = int(result[0])
         file = result[1]
-        if file not in list(dict_blobs):
-            dict_blobs[file] = Blob("f", file, curr_blob.name)
-        dict_blobs[file].size = size
+        if (file, new_depth) not in list(dict_blobs):
+            dict_blobs[(file, new_depth)] = Blob("f", file, curr_blob.name, new_depth)
+        dict_blobs[(file, new_depth)].size = size
         # We also want to denote that the file is a child of the current blob.
-        curr_blob.children.add(file)
+        curr_blob.children.add((file, new_depth))
 
-    return dict_blobs, curr_blob
+    return dict_blobs, curr_blob, depth
 
 
 def solve(filename):
     lines = open(filename, encoding="utf-8").read().splitlines()
 
     curr_blob = None
+    depth = 0
     dict_blobs = {}
     ls_mode = False
     for line in lines:
-        # print("-" * 40)
-        # print(line)
         if line.startswith("$ cd"):
             ls_mode = False
-            dict_blobs, curr_blob = process_cd(line, dict_blobs, curr_blob)
+            dict_blobs, curr_blob, depth = process_cd(line, dict_blobs, curr_blob, depth)
         elif line.startswith("$ ls"):
             ls_mode = True
         elif ls_mode == True:
-            dict_blobs, curr_blob = process_ls(line, dict_blobs, curr_blob)
+            dict_blobs, curr_blob, depth = process_ls(line, dict_blobs, curr_blob, depth)
 
+
+    print("=" * 30)
+    for k, v in dict_blobs.items():
+        print(k, v.__dict__)
+    print("=" * 30)
 
     # Traverse the tree until we have all the directory sizes.
-    queue = [v.name for v in dict_blobs.values() if v.size == -1]
+    queue = [k for k, v in dict_blobs.items() if v.size == -1]
     while len(queue) > 0:
         # Find a node whose children all have sizes.
         for q in queue:
@@ -107,12 +104,18 @@ def mini_test():
     filename = "input-test.txt"
     assert solve(filename) == 95437
 
+    # Same input as above but we changed folder "e" to "d" to have dupe folder
+    # name. The answer should be the same.
+    filename = "input-test2.txt"
+    assert solve(filename) == 95437
+
 
 if __name__ == "__main__":
-    # mini_test()
+    mini_test()
 
     filename = "input.txt"
     total = solve(filename)
 
     print(total)
     # 551163 is too low.
+    # 757593 is too low T_T
