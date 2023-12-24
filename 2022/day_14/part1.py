4b5e6dcdf89bf5da6a5f6b3bb313f82118f42682
jovisly <j@ovisly.com>
2023-12-24 15:12:48 -0800

    can add sand

--- 2022/day_14/part1.py
+++ 2022/day_14/part1.py
@@ -1,6 +1,7 @@
 import ast
+import time
 from curses import wrapper
-from viz import render_rocks
+from viz import render_rocks, update_sand
 
 VIZ = True
 
@@ -37,13 +38,65 @@ def fill_rocks(rock):
     return new_rock
 
 
+
+def move_sand(stdscr, x, y, new_pos):
+    if VIZ:
+        time.sleep(0.1)
+        update_sand(stdscr, (x, y), new_pos)
+    return new_pos
+
+
+
+def make_one_sand(stdscr, all_rocks, all_sands, max_y):
+    x, y = (500, 0)
+    stuck = False
+
+    while stuck is False:
+        d = (x, y + 1)
+        dl = (x - 1, y + 1)
+        dr = (x + 1, y + 1)
+        if d not in all_rocks and d not in all_sands and d[1] <= max_y:
+            # Move down.
+            x, y = move_sand(stdscr, x, y, d)
+        elif dl not in all_rocks and dl not in all_sands and dl[1] <= max_y:
+            # Move down left.
+            x, y = move_sand(stdscr, x, y, dl)
+        elif dr not in all_rocks and dr not in all_sands and dr[1] <= max_y:
+            # Move down right.
+            x, y = move_sand(stdscr, x, y, dr)
+        else:
+            # Stuck.
+            stuck = True
+
+
+    # Only return the sand position if it can stay on the map.
+    return (x, y) if y < max_y else None
+
+
+
 def solve(filename, stdscr):
     lines = open(filename, encoding="utf-8").read().splitlines()
     rocks = [parse_line(l) for l in lines]
     filled_rocks = [fill_rocks(r) for r in rocks]
 
+    # filled rocks is a list of lists of tuples. We will flatten it to a list of
+    # tuples so that we can turn it into a set for faster membership check.
+    all_rocks = set()
+    for r in filled_rocks:
+        for pos in r:
+            all_rocks.add(pos)
+
     if VIZ:
         render_rocks(stdscr, filled_rocks, num_sand=0)
+
+    # No point in keeping the sand if it goes below the lowest rock. Nothing to
+    # catch it.
+    max_y = max([r[1] for r in all_rocks])
+    all_sands = set()
+    sand_pos = make_one_sand(stdscr, all_rocks, all_sands, max_y)
+    all_sands.add(sand_pos)
+    sand_pos = make_one_sand(stdscr, all_rocks, all_sands, max_y)
+
 
     return 0
 
--- 2022/day_14/viz.py
+++ 2022/day_14/viz.py
@@ -1,18 +1,18 @@
 X_OFFSET = 486
 Y_OFFSET = 4
 
+MIN_X = 486
+MAX_X = 505
+MIN_Y = 0
+MAX_Y = 10
 
-def render_rocks(stdscr, rocks, num_sand, min_x=490, max_x=505, min_y=0, max_y=10):
+
+def render_rocks(stdscr, rocks, num_sand, min_x=MIN_X, max_x=MAX_X, min_y=MIN_Y, max_y=MAX_Y):
     stdscr.clear()
 
     for x in range(min_x, max_x + 1):
         for y in range(min_y, max_y + 1):
             stdscr.addstr(y + Y_OFFSET, x - X_OFFSET, ".")
-
-    # Add sand starting position.
-    x, y = (500, 0)
-    stdscr.addstr(y + Y_OFFSET, x - X_OFFSET, "+")
-
 
     # Add rocks.
     for r in rocks:
@@ -28,5 +28,23 @@ def render_rocks(stdscr, rocks, num_sand, min_x=490, max_x=505, min_y=0, max_y=1
         f"Num sand: {num_sand}"
     )
 
+
+    stdscr.refresh()
+
+
+def update_sand(stdscr, old_pos, new_pos, min_x=MIN_X, max_x=MAX_X, min_y=MIN_Y, max_y=MAX_Y):
+    # Old Pos should revert to empty.
+    x, y = old_pos
+    stdscr.addstr(y + Y_OFFSET, x - X_OFFSET, ".")
+    # New Pos should be a sand.
+    x, y = new_pos
+    stdscr.addstr(y + Y_OFFSET, x - X_OFFSET, "o")
+
+    # Move cursor out of the way.
+    stdscr.addstr(
+        max_y + Y_OFFSET + 2,
+        min_x - X_OFFSET + 20,
+        ""
+    )
 
     stdscr.refresh()
