94a56bf88b5e4ea7a0c0244e44df0cadbc710730
jovisly <j@ovisly.com>
2023-12-23 23:41:02 -0800

    complete part 2

--- 2022/day_09/part2.py
+++ 2022/day_09/part2.py
@@ -1,48 +1,61 @@
+import os
 import time
-from curses import wrapper
+
 from part1 import get_instruction, take_one_step, get_avg
 
 
 def move_tail(h_pos, t_pos):
-    """A variation to part 1's move_tail() that breaks down steps."""
-    # print("h_pos", h_pos, "t_pos", t_pos)
+    """A variation to part 1's move_tail() that adheres better the instruction:
+
+    "if the head and tail aren't touching and aren't in the same row or column, the tail always moves one step diagonally to keep up."
+    """
     if (h_pos[0] == t_pos[0] and abs(h_pos[1] - t_pos[1]) == 2):
         # Head and tail are on the same row or column, and there is one cell
         # between them.
-        return [(t_pos[0], get_avg(h_pos[1], t_pos[1]))]
+        return (t_pos[0], get_avg(h_pos[1], t_pos[1]))
     elif (h_pos[1] == t_pos[1] and abs(h_pos[0] - t_pos[0]) == 2):
-        return [(get_avg(h_pos[0], t_pos[0]), t_pos[1])]
-    elif (abs(h_pos[0] - t_pos[0]) == 2 and abs(h_pos[1] - t_pos[1]) == 1):
-        return [
-            (t_pos[0] , h_pos[1]),
-            (get_avg(h_pos[0], t_pos[0]) , h_pos[1])
-        ]
-    elif (abs(h_pos[0] - t_pos[0]) == 1 and abs(h_pos[1] - t_pos[1]) == 2):
-        return [
-            (h_pos[0], t_pos[1]),
-            (h_pos[0], get_avg(h_pos[1], t_pos[1]))
-        ]
+        return (get_avg(h_pos[0], t_pos[0]), t_pos[1])
+    elif h_pos[0] != t_pos[0] and h_pos[1] != t_pos[1] and (
+        abs(h_pos[0] - t_pos[0]) > 1 or abs(h_pos[1] - t_pos[1]) > 1
+    ):
+        d0 = 1 if h_pos[0] > t_pos[0] else -1
+        d1 = 1 if h_pos[1] > t_pos[1] else -1
+        # Move diagonally in the direction of the head
+        return (t_pos[0] + d0, t_pos[1] + d1)
     else:
         # Don't trigger movement if it doesn't need to move.
-        return None
+        return t_pos
 
 
 
-def viz(stdscr, positions, offset_x=10, offset_y=10):
-    stdscr.clear()
-    for i, pos in enumerate(positions):
-        y, x = pos
-        label = "H" if i == 0 else str(i)
-        stdscr.addstr(
-            y + offset_y,
-            x + offset_x,
-            label
-        )
-    stdscr.refresh()
+def viz(positions, dir, dist, y_min=-20, y_max=5, x_min=-11, x_max=25):
+    time.sleep(0.1)
+    os.system('clear')
+    print("\n")
+    print(f" == {dir} {dist} ==")
+    print()
+
+    # Have a reversed version of positions.
+    reversed_positions = positions[::-1]
+    dict_positions = {
+        pos: "H" if i == len(reversed_positions) - 1 else str(len(reversed_positions) - i - 1)
+        for i, pos in enumerate(reversed_positions)
+    }
+    for y in range(y_min, y_max + 1):
+        row = []
+        for x in range(x_min, x_max + 1):
+            if (y, x) in dict_positions:
+                # Find the index of the position.
+                row.append(dict_positions[(y, x)])
+            else:
+                row.append(".")
+        print(" ", "".join(row))
+    print()
 
 
 
-def solve(filename, stdscr):
+
+def solve(filename):
     lines = open(filename, encoding="utf-8").read().splitlines()
     instructions = [get_instruction(line) for line in lines]
     origin = (0, 0)
@@ -51,56 +64,33 @@ def solve(filename, stdscr):
     visited = set()
 
     for dir, dist in instructions:
-        # print("***** INSTRUCTION *****", dir, dist)
         for _ in range(dist):
-            # queue = [(0, take_one_step(positions[0], dir))]
-            dict_queue = {i: [] for i in range(len(positions))}
-            dict_queue[0] = [take_one_step(positions[0], dir)]
-            while sum([len(v) for v in dict_queue.values()]) > 0:
-                # Pick the queue with the biggest index with none zero length.
-                ind = max([k for k, v in dict_queue.items() if len(v) > 0])
-                queue = dict_queue[ind]
-                # Take the first element.
-                pos = queue[0]
-                dict_queue[ind] = queue[1:]
-
-                if ind == len(positions) - 1:
-                    visited.add(pos)
-
-                # Update the position according to queue.
-                positions[ind] = pos
-                time.sleep(1)
-                viz(stdscr, positions)
-                next_ind = ind + 1
-                if next_ind < len(positions):
-                    h_pos = positions[ind]
-                    t_pos = positions[next_ind]
-                    list_new_pos = move_tail(h_pos, t_pos)
-                    if list_new_pos is not None:
-                        dict_queue[next_ind] = list_new_pos
+            for i in range(len(positions)):
+                if i == 0:
+                    # Move head
+                    positions[i] = take_one_step(positions[i], dir)
+                else:
+                    # Move tail
+                    positions[i] = move_tail(positions[i - 1], positions[i])
+            # viz(positions, dir, dist)
+            visited.add(positions[-1])
 
     # print("len", len(visited))
     return len(visited)
 
 
-def mini_test(stdscr):
+def mini_test():
     filename = "input-test.txt"
-    assert solve(filename, stdscr) == 1
+    assert solve(filename) == 1
 
-    # filename = "input-test2.txt"
-    # assert solve(filename) == 36
-
-
-def main(stdscr):
-    filename = "input-test.txt"
-    solve(filename, stdscr)
-    # mini_test(stdscr)
-
-    # filename = "input.txt"
-    # total = solve(filename)
-
-    # print(total)
-    stdscr.getch()
+    filename = "input-test2.txt"
+    assert solve(filename) == 36
 
 
-wrapper(main)
+if __name__ == "__main__":
+    mini_test()
+
+    filename = "input.txt"
+    total = solve(filename)
+
+    print(total)
