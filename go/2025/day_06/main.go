7a5ac82a999fcdb904fb2f645bbb2720b48b9d16
jovisly <j@ovisly.com>
2025-12-05 21:43:17 -0800

    2025d6p2

--- go/2025/day_06/main.go
+++ go/2025/day_06/main.go
@@ -1,3 +1,4 @@
+// The question that makes me miss python :p
 package main
 
 import (
@@ -28,7 +29,7 @@ func processLinesP1(lines []string) int {
 		processedLines = append(processedLines, cleanedSegs)
 	}
 
-	// fmt.Println(processedLines)
+	// fmt.Printf("%#v\n", processedLines)
 
 	ops := processedLines[len(processedLines)-1]
 	tot := 0
@@ -58,9 +59,86 @@ func processLinesP1(lines []string) int {
 	return tot
 }
 
+func processLinesP2(lines []string) int {
+	opsString := lines[len(lines)-1]
+	ops := strings.Split(opsString, " ")
+	ops = cleanStrings(ops)
+	// Reverse
+	for i, j := 0, len(ops)-1; i < j; i, j = i+1, j-1 {
+		ops[i], ops[j] = ops[j], ops[i]
+	}
+
+	// fmt.Printf("%#v\n", ops)
+
+	numsStrings := lines[:len(lines)-1]
+	// fmt.Printf("%#v\n", numsStrings)
+
+	// We need to pad the numsStrings to equal length by adding space to the end.
+	maxLen := 0
+	for _, numsString := range numsStrings {
+		if len(numsString) > maxLen {
+			maxLen = len(numsString)
+		}
+	}
+	for i, numsString := range numsStrings {
+		numsStrings[i] = numsString + strings.Repeat(" ", maxLen-len(numsString))
+	}
+
+	// fmt.Printf("%#v\n", numsStrings)
+
+	// Then we start to build the array of arrays for the numbers; do it in the reverse
+	// order.
+	numss := [][]string{}
+	nums := []string{}
+	for i := maxLen - 1; i >= 0; i-- {
+		currNum := ""
+		for _, numsString := range numsStrings {
+			currNum += string(numsString[i])
+		}
+		// fmt.Printf("%#v\n", currNum)
+		trimmed := strings.TrimSpace(currNum)
+		if len(trimmed) == 0 {
+			numss = append(numss, nums)
+			nums = []string{}
+		} else {
+			nums = append(nums, trimmed)
+		}
+	}
+	numss = append(numss, nums)
+	// fmt.Printf("%#v\n", numss)
+
+	tot := 0
+
+	for i, op := range ops {
+		val := 0
+		if op == "*" {
+			val = 1
+		}
+
+		nums := numss[i]
+		for _, num := range nums {
+			n, err := strconv.Atoi(num)
+			if err != nil {
+				panic(err)
+			}
+			if op == "*" {
+				val *= n
+			}
+			if op == "+" {
+				val += n
+			}
+		}
+
+		tot += val
+	}
+
+	return tot
+}
+
 func main() {
 	lines := utils.ReadLines(FILENAME)
+	// fmt.Printf("%#v\n", lines)
 
 	fmt.Println("Part 1: ", processLinesP1(lines))
-	// fmt.Println("Part 2: ", len(lines))
+	fmt.Println("Part 2: ", processLinesP2(lines))
 }
